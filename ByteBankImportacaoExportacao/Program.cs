115ea03f740669b26e3a0f0e9191bc7642b5390a
Wesley <wesley.contato@hotmail.com>
2020-11-25 06:17:16 -0300

    Convertendo o texto para contacorrente

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -22,12 +22,42 @@ namespace ByteBankImportacaoExportacao
                 while (!leitor.EndOfStream)
                 {
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                    var msg = ($"Conta número {contaCorrente.Numero}, Agência: {contaCorrente.Agencia}, Saldo: {contaCorrente.Saldo}");
+                    Console.WriteLine(msg);
+                    //Console.WriteLine(linha);
 
                 }
             }
 
             Console.ReadLine();
         }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(' ');
+
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            var agenciaComoInt = int.Parse(agencia);
+            var numeroComoInt = int.Parse(numero);
+            var saldoComoDouble = double.Parse(saldo);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+            resultado.Depositar(saldoComoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
+
+
+
     }
 }
