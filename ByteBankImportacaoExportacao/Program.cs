5c00cf39e03567ddd1932389fe084ab00433dc33
Wesley <wesley.contato@hotmail.com>
2020-11-25 04:45:59 -0300

    Criando um FileStream

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ByteBankImportacaoExportacao 
 { 
@@ -11,7 +12,34 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+            var numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos !=0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            
+
+
+
+
             Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meyByte in buffer)
+            {
+                Console.Write(meyByte);
+                Console.Write(" ");
+            }
         }
     }
 } 
